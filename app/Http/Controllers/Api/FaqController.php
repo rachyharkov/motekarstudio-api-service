1c77cb899dd1510562808f831fb21046afd7304d
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2023-02-21 18:05:08 +0700

    upload vendor

--- app/Http/Controllers/Api/FaqController.php
+++ app/Http/Controllers/Api/FaqController.php
@@ -3,9 +3,37 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Faq;
 use Illuminate\Http\Request;
 
 class FaqController extends Controller
 {
-    //
+    public function getAll()
+    {
+        // if request is via API, return JSON
+
+        $arrheader = [];
+
+        if(env('ALLOW_CORS') == true) {
+            $arrheader = [
+                "Access-Control-Allow-Origin" => '*',
+            ];
+        }
+
+        if (request()->wantsJson()) {
+            return response()->json(
+                Faq::select('question', 'answer')->get(),
+                200,
+                $arrheader,
+                JSON_UNESCAPED_UNICODE
+            );
+        }
+
+        return response()->json(
+            [
+                'message' => 'This route is only available via API.',
+            ],
+            404
+        );
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\FaqController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('faq')->controller(FaqController::class)->group(function() {
+    Route::get('/', [FaqController::class, 'getAll']);
 });
